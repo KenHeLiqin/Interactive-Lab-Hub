b08685b7994f1f5eb3da60ee38bfd59da0fa9472
KH <liqin.he.ken@gmail.com>
2021-09-26 23:24:11 -0400

    put pictures

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -77,29 +77,31 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
-
-image = Image.open(cwd + "/sun_pic/" + "1" +".jpg")
-
 cwd = os.getcwd()
 
-# Scale the image to the smaller screen dimension
-image_ratio = image.width / image.height
-screen_ratio = width / height
-if screen_ratio < image_ratio:
-    scaled_width = image.width * height // image.height
-    scaled_height = height
-else:
-    scaled_width = width
-    scaled_height = image.height * width // image.width
-image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+while True:
 
-# Crop and center the image
-x = scaled_width // 2 - width // 2
-y = scaled_height // 2 - height // 2
-image = image.crop((x, y, x + width, y + height))
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    image = Image.open(cwd + "/sun_pic/" + "1" +".jpg")
 
-# Display image.
-disp.image(image)
+    # Scale the image to the smaller screen dimension
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
+
+    # Display image.
+    disp.image(image)
 
 
 # # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
